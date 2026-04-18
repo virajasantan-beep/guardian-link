6c32805d70a1d5b1086d7c1e42eae5643b0bbfa2
virajasantan-beep <viraja.santan@gmail.com>
2026-04-18 09:29:58 +0530

    Delete guardianlink directory

--- guardianlink/backend/instagram_api.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-def fetch_mock_messages():
-    with open(os.path.join(BASE_DIR, "mock_data.json")) as f:
-        return json.load(f)
